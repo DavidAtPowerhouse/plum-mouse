7a8a34bd634b7b954ff94c10268d1393fa5b15fd
David Wong <david.wong@maas.museum>
2023-01-25 15:42:19 +1100

    Update schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -3,17 +3,19 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
-
+import blogPost from './documents/blogPost'
 import animal from './animal'
 import article from './documents/article'
 import category from './documents/category'
+import imageResource from "./documents/imageResource";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-	  animal,
-	  article,
-	  category
+    blogPost,
+    article,
+    category,
+    imageResource
   ]),
 })
